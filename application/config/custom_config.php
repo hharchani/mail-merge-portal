03320fa718b62ffa551d4301f43dc67e24ab1d76
Harshit Harchani <hharchani@gmail.com>
2015-10-08 11:37:02 +0530

    Added common config, db config, autoload, changed default controller

---
+++ application/config/custom_config.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Upload Directory Name
+|--------------------------------------------------------------------------
+*/
+
+$config['upload_dir'] = APPPATH . 'uploads';
